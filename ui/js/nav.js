b11f74634154f5ef9793892c075d1e4524f678ff
gaganjot99 <gagankahal00@gmail.com>
2022-04-24 12:54:44 +0530

    feat: nav bar shows loggedin user in index main and addinfo pages

--- ui/js/nav.js
+++ ui/js/nav.js
@@ -1,24 +1,55 @@
 //functions to toggle the dropdown list from the nav bar
-var man_btn = document.getElementById("man_dd");
-var nav_dd = document.getElementsByClassName("nav_dd")[0];
-var arrowfils = document.getElementsByClassName("arrow_nav")[0];
-var stat1 = "false";
-man_btn.addEventListener("click", (e) => {
-  if (stat1 == "false") {
-    nav_dd.style.visibility = "visible";
-    arrowfils.style.visibility = "visible";
-    stat1 = "true";
-  } else {
-    nav_dd.style.visibility = "hidden";
-    arrowfils.style.visibility = "hidden";
-    stat1 = "false";
-  }
-});
+var man_btn = document.querySelectorAll(".man_dd");
+var nav_dd = document.getElementsByClassName("nav_dd");
+var arrowfils = document.getElementsByClassName("arrow_nav");
+var stat1 = false;
 
-document.body.addEventListener("click", (e) => {
-  if (!nav_dd.contains(e.target) && !man_btn.contains(e.target)) {
-    nav_dd.style.visibility = "hidden";
-    arrowfils.style.visibility = "hidden";
-    stat1 = "false";
-  }
-});
+if (man_btn[0]) {
+  man_btn.forEach((ele, i) => {
+    ele.addEventListener("click", (e) => {
+      if (stat1 === false || stat1 != i) {
+        console.log("what I'm here for");
+        nav_dd[i].style.visibility = "visible";
+        arrowfils[i].style.visibility = "visible";
+        stat1 = i;
+      } else {
+        console.log("i'm here");
+        nav_dd[i].style.visibility = "hidden";
+        arrowfils[i].style.visibility = "hidden";
+        stat1 = false;
+      }
+    });
+  });
+
+  document.body.addEventListener("click", (e) => {
+    if (stat1 !== false) {
+      if (
+        !nav_dd[stat1].contains(e.target) &&
+        !man_btn[stat1].contains(e.target)
+      ) {
+        for (let i = 0; i < nav_dd.length; i++) {
+          nav_dd[i].style.visibility = "hidden";
+          arrowfils[i].style.visibility = "hidden";
+        }
+        stat1 = false;
+      }
+    }
+  });
+}
+
+const signBtn = document.getElementById("sign_btn");
+const userDiv = document.getElementById("user_nav");
+
+if (userDiv) {
+  fetch("/userinfo")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.firstname) {
+        userDiv.children[0].src = data.image;
+        userDiv.children[1].innerText = data.firstname;
+        signBtn.style.display = "none";
+        userDiv.style.display = "flex";
+      }
+    })
+    .catch((err) => console.log(err));
+}
